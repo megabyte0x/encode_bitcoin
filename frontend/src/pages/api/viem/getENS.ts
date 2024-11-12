3ebfd510d5ded2a315401da5ef34e52f3d5551e1
megabyte0x <yashygkf@gmail.com>
2024-11-12 08:25:46 +0700

    feat: generate namehash

--- frontend/src/pages/api/viem/getENS.ts
+++ frontend/src/pages/api/viem/getENS.ts
@@ -1,7 +1,11 @@
 import { client } from "./client"
-export const ensName = async (user_address: any) => {
+import { namehash, normalize } from 'viem/ens'
+
+export const getENSName = async (user_address: any) => {
     let _address = user_address.startsWith('0x') ? user_address : "";
     return await client.getEnsName({
         address: _address
     })
 };
+
+export const getNamehash = (ens: string) => namehash(ens);
